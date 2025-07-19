5f412f9c1e415095a664f31aa7a64cb714696b45
fsociety00d4t <58801059+fsociety00d4t@users.noreply.github.com>
2025-07-19 23:58:13 +0300

    cards hover efffect

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,10 +2,32 @@ import PropTypes from 'prop-types';
 
 function Cards({ images, handleClick, animateAll, onAnimationEnd }) {
   
+const handleCardTilt = (e) => {
+  const card = e.currentTarget;
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left; // mouse X inside card
+  const y = e.clientY - rect.top;  // mouse Y inside card
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = -(y - centerY) / 5; // Negative to invert direction
+  const rotateY = (x - centerX) / 5;
+
+  card.style.transform = `perspective(600px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+};
+
+const resetTilt = (e) => {
+  const card = e.currentTarget;
+  card.style.transform = "perspective(600px) rotateX(0deg) rotateY(0deg)";
+};
+
   return (
     <div className="cards-container">
       {images.map((image) => (
         <img
+         onMouseMove={handleCardTilt}
+         onMouseOut={resetTilt}
          className={`card ${animateAll ? 'clicked-card' : ''}`}
           src={image.src}
           alt={image.name}
